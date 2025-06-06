d2607648ec343fe99d279fe11200db449d318efd
rodrigodornelles <rodrigo.dornelles.poa@gmail.com>
2025-06-06 14:11:54 -0300

    fix: MultiLineStatement: Statement spans multiple lines

--- source/cli/tools/meta.lua
+++ source/cli/tools/meta.lua
@@ -147,9 +147,11 @@ local function vars(args)
 end
 
 local function render(infile, content, args, optional)
-    local game = normalize_table(
-        try_table(infile) or try_lua(infile) or try_decode(infile, json) or try_decode(infile, env) or 
-        try_tic80(infile)
+    local game = normalize_table(try_table(infile)
+        or try_lua(infile)
+        or try_decode(infile, json)
+        or try_decode(infile, env)
+        or try_tic80(infile)
     ) or (optional and {})
 
     if not game then 
