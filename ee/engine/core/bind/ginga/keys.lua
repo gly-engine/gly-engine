939ec6f95e4c1e790ebebc43de736b0a63ffa353
DornellesTV <rodrigo.dornelles.poa@gmail.com>
2025-09-09 17:44:41 -0300

    hotfix: add debounce for lg magic control 2024 #196 (#231)
    * docs: more info bug telemedia 190
    
    * fix: hardfix 196
    
    * fix: improve fixes #196
    
    * style: one statement per line in keys

--- ee/engine/core/bind/ginga/keys.lua
+++ ee/engine/core/bind/ginga/keys.lua
@@ -16,11 +16,21 @@ local key_bindings={
     ENTER='a'
 }
 
---! @li https://github.com/TeleMidia/ginga/issues/190
+local fixture_196_key = ''
+local pressed_196_key = ''
+local fixture_196_looped = false
+
 local function event_ginga(std, evt)
     if evt.class ~= 'key' then return end
     if not key_bindings[evt.key] then return end
-    if evt.key == 'BACK' then
+
+    local pressed = evt.type == 'press'
+    local raw_key = evt.key
+    local gly_key = key_bindings[evt.key]
+    local is_back = raw_key == 'BACK'
+    local is_back_or_red = is_back or raw_key == 'RED'
+
+    if is_back then
         event.post('out', {
             class = 'ncl',
             type = 'edit',
@@ -30,12 +40,37 @@ local function event_ginga(std, evt)
             value = 'application'
         })
     end
-    std.bus.emit('rkey', key_bindings[evt.key], evt.type == 'press')
 
+    --! @li @li https://github.com/gly-engine/gly-engine/issues/196
+    --! Fix ensures at least syncing a loop when the button is pressed.
+    if is_back_or_red and pressed then pressed_196_key = gly_key end
+    if is_back_or_red and not pressed and pressed_196_key == gly_key then 
+        fixture_196_key = gly_key
+        return
+    end
+
+    --! @li https://github.com/TeleMidia/ginga/issues/190
+    --! this condtional is inverse in ---dev building flag.
+    std.bus.emit('rkey', gly_key, pressed)
+end
+
+local function event_fixed(std)
+    if #fixture_196_key > 0 then
+        if not fixture_196_looped then
+            fixture_196_looped = true
+        else
+            std.bus.emit('rkey', fixture_196_key, 0)
+            fixture_196_looped = false
+            fixture_196_key = ''
+        end
+    else
+        pressed_196_key = ''
+    end
 end
 
 local function install(std)
     std.bus.listen_std('ginga', event_ginga)
+    std.bus.listen_std('loop', event_fixed)
 end
 
 local P = {
