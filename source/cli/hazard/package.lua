7eaefe5f82c90203b177bb436fc5b5c0830dbf1d
RodrigoDornelles <rodrigo.dornelles.poa@gmail.com>
2025-10-23 00:17:14 -0300

    chore: rewrite module_mock

--- source/cli/hazard/package.lua
+++ source/cli/hazard/package.lua
@@ -1,5 +1,4 @@
 local function module_mock(src_in, mock_in, module_name)
-    local content = ''
     local src_file, src_err = io.open(src_in, 'r')
     local mock_file, mock_err = io.open(mock_in, 'r')
 
@@ -7,32 +6,30 @@ local function module_mock(src_in, mock_in, module_name)
         return false, src_err or mock_err
     end
 
-    local in_module = false
-    local pattern1 = module_name..'_%w+ = function'
-    local pattern2 = '%-%-'
-
-    repeat
-        local line = src_file:read()
-
-        if line then
-            if not in_module then
-                content = content..line..'\n'
-            end
-
-            if not in_module and line:find(pattern1) then
-                content = content..mock_file:read('*a')
-                in_module = true
-            end
-
-            if in_module and line:find(pattern2) then
-                content = content..'end\n-'..'-\n'
-                in_module = false
-            end
-        end
-    until not line
-
+    local content = src_file:read('*a')
     src_file:close()
-    src_file = io.open(src_in, 'wb')
+    
+    local id_bundler = (content:match('local (b[%d%a]+)%s*=%s*%{%s*0[%s,0]*%}\n') or ''):sub(2)
+    if #id_bundler == 0 then return true, 'is not a bundler!' end
+
+    local id_module = content:match(('b{1}%[(%d+)%]%(\'{2}\'%)'):gsub('{1}', id_bundler):gsub('{2}', module_name))
+    if not id_module then return false, 'module not found!' end
+
+    local next_id_module = tonumber(id_module) + 1
+    local pattern_start = ('b{1}%[{2}%] = r{1}%({2}, function%(%)'):gsub('{1}', id_bundler):gsub('{2}', id_module)
+    local pattern_end_1 = ('\nend%)\nb{1}%[{2}%] = r{1}%({2}, function%(%)'):gsub('{1}', id_bundler):gsub('{2}', next_id_module)
+    local pattern_end_2 = ('\nend%)\nreturn m{1}%(%)'):gsub('{1}', id_bundler)
+    local pos_end = content:find(pattern_end_1) or content:find(pattern_end_2)
+    local pos_start = content:find(pattern_start)
+    local header = pattern_start:gsub('%%', '')
+
+    if not pos_end or not pos_end then return false, 'some error!' end
+
+    local module_content = mock_file:read('*a')
+    content = content:sub(1, pos_start - 1)..header..module_content..content:sub(pos_end, #content)
+    mock_file:close()
+    
+    src_file = io.open(src_in, 'w')
     src_file:write(content)
     src_file:close()
 
--- tools/ci_luau-analyze.lua
+++ tools/ci_luau-analyze.lua
@@ -9,9 +9,9 @@ if core == 'cli' then
     cmd(replace..' --format "string.dump" --replace "string.format"')
     cmd(replace..' --format "arg = {args.game}" --replace ""')    
     cmd(replace..' --format "arg = nil" --replace ""')    
-    cmd('./cli.sh hazard-package-mock tests/mock/json.lua dist/main.lua source_third_party_rxi_json')
-    cmd('./cli.sh hazard-package-mock tests/mock/lustache.lua dist/main.lua source_third_party_olivinelabs_lustache')
-    cmd('./cli.sh hazard-package-mock tests/mock/zlib.lua dist/main.lua source_third_party_zerkman_zlib')
+    cmd('./cli.sh hazard-package-mock tests/mock/json.lua dist/main.lua source/third_party/rxi_json')
+    cmd('./cli.sh hazard-package-mock tests/mock/lustache.lua dist/main.lua source/third_party/olivinelabs_lustache')
+    cmd('./cli.sh hazard-package-mock tests/mock/zlib.lua dist/main.lua source/third_party/zerkman_zlib')
     return
 end
 
@@ -26,4 +26,4 @@ else
     cmd('./cli.sh build-engine --bundler @'..core)
 end
 cmd(replace..' --format "function native_callback" --replace "local function _native_callback"')
-cmd('./cli.sh hazard-package-mock tests/mock/json.lua dist/main.lua source_third_party_rxi_json')
+cmd('./cli.sh hazard-package-mock tests/mock/json.lua dist/main.lua source/third_party/rxi_json')
