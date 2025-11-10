bd020da7d3e99d599bbc4840c1916fbf26ff3198
RodrigoDornelles <rodrigo.dornelles.poa@gmail.com>
2025-11-10 20:55:30 -0300

    feat: some changes in dvdplayer sample

--- samples/dvdplayer/game.lua
+++ samples/dvdplayer/game.lua
@@ -4,11 +4,13 @@ local function init(self, std)
     self.size = 80
     self.hspeed = self.width/5000
     self.vspeed = self.height/4000
+    self.image = 'icon80x80.png'
+    self.delta = 16
 end
 
 local function loop(self, std)
-    self.x = self.x + (self.hspeed * std.delta)
-    self.y = self.y + (self.vspeed * std.delta)
+    self.x = self.x + (self.hspeed * self.delta)
+    self.y = self.y + (self.vspeed * self.delta)
     if self.x <= 1 or self.x >= self.width - self.size then
         self.hspeed = -1 * self.hspeed
     end
@@ -19,7 +21,7 @@ end
 
 local function draw(self, std)
     std.draw.clear(std.color.black)
-    std.image.draw('icon80x80.png', self.x, self.y)
+    std.image.draw(self.image, self.x, self.y)
 end
 
 local function exit(self, std)
