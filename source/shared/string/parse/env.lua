87c129e64c68668169e498d2301df4e2e6936c7a
RodrigoDornelles <rodrigo.dornelles.poa@gmail.com>
2025-11-12 18:53:21 -0300

    feat: improve dotenv parse to allow comments and quotes

--- source/shared/string/parse/env.lua
+++ source/shared/string/parse/env.lua
@@ -33,17 +33,40 @@ end
 
 local function decode(str)
     local res = {}
+    local lineno = 0
+
     for line in str:gmatch("[^\r\n]+") do
+        lineno = lineno + 1
         local l = line:match("^%s*(.-)%s*$")
         if l ~= "" and not l:match("^#") then
-            local k,v = l:match("^([%w_.]+)=(.*)$")
-            if not k or res[k] then
+            local in_single, in_double, pos = false, false, nil
+            for i = 1, #l do
+                local c = l:sub(i, i)
+                if c == "'" and not in_double then
+                    in_single = not in_single
+                elseif c == '"' and not in_single then
+                    in_double = not in_double
+                elseif c == "#" and not in_single and not in_double then
+                    pos = i
+                    i = #l
+                end
+            end
+            if pos then
+                l = l:sub(1, pos - 1)
+            end
+            local k, v = l:match("^%s*([%w_.]+)%s*=%s*(.*)$")
+            if not k then
                 return {}
             end
             v = v:match('^"(.*)"$') or v:match("^'(.*)'$") or v
+            v = v:match("^%s*(.-)%s*$")
+            if res[k] then
+                return {}
+            end
             res[k] = v
         end
     end
+
     return res
 end
 
