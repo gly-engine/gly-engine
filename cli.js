90d80df97bf401c80ca5f23b36c6fc42cb498240
RodrigoDornelles <rodrigo.dornelles.poa@gmail.com>
2025-11-24 15:35:01 -0300

    change: refact gly-cli npm package

--- cli.js
+++ cli.js
@@ -1,24 +1,20 @@
 #!/usr/bin/env node
 const { execFileSync } = require('child_process');
-const { existsSync, renameSync } = require('fs');
+const { existsSync } = require('fs');
 const { resolve } = require('path');
 
-const GLY_PATH = resolve('npm', 'gly-cli');
-const GLY_CLI = resolve(GLY_PATH, 'src', 'index.ts');
-const GLY_VENDOR = resolve(GLY_PATH, 'node_modules');
+const GLY_CLI = resolve('npm', 'gly-cli', 'index.ts');
 const ROOT_VENDOR = resolve('node_modules');
 
 try {
-  if (!existsSync(GLY_VENDOR) && !existsSync(ROOT_VENDOR)) {
+  if (!existsSync(ROOT_VENDOR)) {
     execFileSync('npm', ['install', '--no-package-lock'], {
       shell: true,
-      cwd: GLY_PATH,
       env: process.env,
       stdio: 'inherit'
     });
-    renameSync(GLY_VENDOR, ROOT_VENDOR);
   }
-  execFileSync('npx', ['--prefix', GLY_PATH, 'bun', GLY_CLI, ...process.argv.slice(2)], { 
+  execFileSync('npx', ['bun', GLY_CLI, ...process.argv.slice(2)], { 
     shell: true,
     env: process.env,
     stdio: 'inherit'
