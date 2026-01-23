30a64cefc940f1948abbf5250a409112ea1e6bc2
RodrigoDornelles <rodrigo.dornelles.poa@gmail.com>
2026-01-23 10:58:40 -0300

    docs: starts docusaurus

---
+++ website/docs/intro.md
@@ -0,0 +1,23 @@
+---
+sidebar_position: 1
+---
+
+# Powered by Gamely
+
+## Documentation
+
+- Hello World (Coming Soon)
+- Installation (Coming Soon)
+- List of examples (Coming Soon)
+- Platform Support (Coming Soon)
+- [View the CLI/Engine source code](https://github.com/gly-engine/gly-engine)
+- Comparison between Game Engines (Coming Soon)
+
+## Requirements
+
+| specification | CPU      | ram    | description |
+| :------------ | -------: | -----: | :------------ |
+| baseline      | 16.8 MHz | 288 KB | run simplistic applications like an Atari 2600 style game |
+| minimum       | 400 MHz  | 32 MB  | 1080i media and run applications without slowdowns |
+| recommended   | 1.8 Ghz  | 2 GB   | 1080p media and run applications very fast |
+| enhanced      | 3.0 Ghz  | 8 GB   | multiples 1080p medias and run multi applications at same time |
---
+++ website/docusaurus.config.ts
@@ -0,0 +1,128 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+
+const config: Config = {
+  title: 'GlyEngine',
+  tagline: 'Create games and apps with lua',
+  favicon: 'img/logo.png',
+
+  // Set the production url of your site here
+  url: 'https://gly-engine.github.io',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/gly-engine/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'gly-engine', // Usually your GitHub org/user name.
+  projectName: 'gly-engine', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  markdown: {
+    mermaid: true,
+  },
+  themes: ['@docusaurus/theme-mermaid'],
+
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  stylesheets: [
+    {
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      type: 'text/css',
+      integrity:
+        'sha384-odtC+0BnsA5VcN2wK4EOhEA7rncHrAoc8hfbuclVrS5KnMDEXNNsAulmJ/2QidSH',
+      crossorigin: 'anonymous',
+    },
+  ],
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          editUrl:
+            'https://github.com/gly-engine/gly-engine/tree/main/website/',
+          remarkPlugins: [remarkMath],
+          rehypePlugins: [rehypeKatex],
+        },
+        blog: false,
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    image: 'img/logo.png',
+    navbar: {
+      title: 'GlyEngine',
+      logo: {
+        alt: 'GlyEngine Logo',
+        src: 'img/logo.png',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Docs',
+        },
+        {
+          href: 'https://github.com/gly-engine/gly-engine',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Tutorial',
+              to: '/docs/intro',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Discord',
+              href: 'https://discord.gg/2p5u2a9',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'GitHub',
+              href: 'https://github.com/gly-engine/gly-engine',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} GlyEngine. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+      additionalLanguages: ['lua'],
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
